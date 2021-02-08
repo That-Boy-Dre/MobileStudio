229ac575e7731a07c6cdb024f83211222ecec2f9
Dondre Campbell <Dondrexcampbell@gmail.com>
2021-02-08 10:54:56 -0500

    version 4 (Perfect part 2)

--- School Crap/Car.java
+++ School Crap/Car.java
@@ -2,38 +2,38 @@
 public class Car {
 
   int fuel_efficiency;
-  int mileage;
-  int fuel_tank_capacity;
-  static int fuel_level;
+  double mileage;
+  double fuel_tank_capacity;
+  static double fuel_level;
   int milesDriven;
   
   
-  public Car(int fuel_efficiency, int mileage, int fuel_tank_capacity){
+  public Car(int fuel_efficiency, int mileage, double fuel_tank_capacity){
     this.fuel_efficiency = fuel_efficiency;
     this.mileage = mileage;
     this.fuel_tank_capacity = fuel_tank_capacity;
     fuel_level = 0;
   }
 
-  public void drive(int milesDriven){
-    this.milesDriven = milesDriven;
-    fuel_level = (this.fuel_efficiency * this.fuel_tank_capacity) - this.milesDriven;
-   
-  }
-
-  public static int getFuelLevel(){
-    return fuel_level;
-  }
-
   public void addGas(int gallons){
     fuel_level = fuel_level + gallons;
   }
+  
+  public static double getFuelLevel(){
+    return fuel_level;
+  }
+  
+  
+  public void drive(double milesDriven){
+    fuel_level = fuel_level - (milesDriven / fuel_efficiency);
+    mileage = mileage + milesDriven;
+  }
 
   public void displayCar(){
-    System.out.println("fuel level: "+getFuelLevel());
-    System.out.println("fuel efficiency: "+this.fuel_efficiency+" mpg");
-    System.out.println("fuel tank capacity: "+this.fuel_tank_capacity+" gallons");
-    System.out.println("mileage: "+(this.mileage + milesDriven));
+    System.out.println("fuel level: "+getFuelLevel()+" gallons");
+    System.out.println("fuel efficiency: "+fuel_efficiency+" mpg");
+    System.out.println("fuel tank capacity: "+fuel_tank_capacity+" gallons");
+    System.out.println("mileage: "+mileage);
 
   }
 
--- School Crap/TestCar.java
+++ School Crap/TestCar.java
@@ -4,8 +4,8 @@ public class TestCar {
   
 
   public static void main(String[] args) {
-    Car bmw = new Car(25, 15000, 15);
-    bmw.addGas(10);
+    Car bmw = new Car(25, 10000, 15);
+    bmw.addGas(12);
     bmw.drive(130);
     bmw.displayCar();
   }
