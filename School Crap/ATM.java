aaad1a8cf1c0ecbbd043453acca5d4bcf1fa8722
Dondre Campbell <Dondrexcampbell@gmail.com>
2021-02-14 10:04:56 -0500

    just a formatting update

--- School Crap/ATM.java
+++ School Crap/ATM.java
@@ -1,7 +1,6 @@
 
 
 import java.util.Scanner;
-
 public class ATM {
 
 
@@ -34,7 +33,7 @@ public class ATM {
     }
     
     // ==========================================================================The menu loop begins here.
-    while(codeNotValid == false){
+    while(codeNotValid == false){ //Begining of while loop
 
       System.out.println("\nPlease select from the following options:");
       System.out.println("1 – View Balance ");
@@ -45,7 +44,7 @@ public class ATM {
       int choice = input.nextInt();
 
 
-      switch (choice) {
+      switch (choice) { //Begining of switch statement
         case 1:
              System.out.println("your balance is "+DondresAccount.getBalance());
              break;
@@ -67,14 +66,54 @@ public class ATM {
         default:
               System.out.println("Error! Invalid input.");
               break;
-    } 
-  } 
-  // ==========================================================================The menu loop begins here.
+    } //End of switch statement
+
+  } //End ofwhile loop 
+
   
   }
 }
 
+/*  Welcome to SJU ATM service! 
+    Please enter a valid pin number:
+    2345565
+    Invalid pin, please re-enter.
+    2345
+    Invalid pin, please re-enter.
+    4567
 
+    Please select from the following options:
+    1 – View Balance
+    2 – Make a Deposit
+    3 – Withdraw Cash
+    4 – Exit
+    1
+    your balance is 2700.0
 
+    Please select from the following options:
+    1 – View Balance
+    2 – Make a Deposit
+    3 – Withdraw Cash
+    4 – Exit
+    2
+    Enter the amount to deposit:
+    300
+
+    Please select from the following options:
+    1 – View Balance
+    2 – Make a Deposit
+    3 – Withdraw Cash
+    4 – Exit
+    1
+    your balance is 3000.0
+
+    Please select from the following options:
+    1 – View Balance
+    2 – Make a Deposit
+    3 – Withdraw Cash
+    4 – Exit
+    4
+    Thank you for using our ATM!
+*/
 
 
