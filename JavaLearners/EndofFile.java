a9d44c90f3ae9e20d06ac8463fd06a3a12c34688
Dondre Campbell <Dondrexcampbell@gmail.com>
2021-02-14 12:54:07 -0500

    option1

--- JavaLearners/EndofFile.java
+++ JavaLearners/EndofFile.java
@@ -6,24 +6,10 @@ public class EndofFile {
   public static void main(String[] args) {
     
     Scanner scan = new Scanner(System.in);
-    String words = "";
-    boolean inputAsked = true;
-    int count = -1;
     
-    while(inputAsked){
-      count++;
-      words = scan.nextLine();
-
-      if(words.equalsIgnoreCase("finish")){
-        inputAsked = false;
-
-      }
+    for(int i = 1; scan.hasNext() == true; i++){
+      System.out.println(i+" "+scan.nextLine());
     }
-    
-    for(int i = 0; i <= count; i++){
-    System.out.println(count+" "+words);
-    }
-    
 
     scan.close();
   }
