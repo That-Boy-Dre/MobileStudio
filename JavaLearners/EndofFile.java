f981a67f6953ab4cdb17dc31c6af72d625141894
Dondre Campbell <Dondrexcampbell@gmail.com>
2021-02-16 15:50:24 -0500

    currency format complete

--- JavaLearners/EndofFile.java
+++ JavaLearners/EndofFile.java
@@ -2,9 +2,9 @@
 
 /*
 
- for all the people who are saying that how will the scanner.hasNext() condition will come false,then listen folks. it is not described here but
- the input is coming from a text file and the scanner object starts to read from the top and reads upto the end. when the end of the text file 
- comes as there is nothing to read the condition become false and loop brakes.
+for all the people who are saying that how will the scanner.hasNext() condition will come false,then listen folks. it is not described here but
+the input is coming from a text file and the scanner object starts to read from the top and reads upto the end. when the end of the text file 
+comes as there is nothing to read the condition become false and loop brakes.
 
 */
 
@@ -13,7 +13,7 @@
 
 
 import java.util.Scanner;
-public class EndofFile {
+public class EndOfFile {
   
 
   public static void main(String[] args) {
@@ -26,20 +26,20 @@ public class EndofFile {
 
     scan.close();
   }
-
 }
 
 /*
-  hello world
-  1 hello world
-  hello world
-  2 hello world
-  hello world
-  3 hello world
-  hello world
-  4 hello world
-  hello world
-  5 hello world
-  hello world
-  6 hello world
+hello world
+1 hello world
+hello world
+2 hello world
+hello world
+3 hello world
+hello world
+4 hello world
+hello world
+5 hello world
+hello world
+6 hello world
 */
+
